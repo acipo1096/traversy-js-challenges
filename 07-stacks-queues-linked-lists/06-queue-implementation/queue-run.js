9b589c49f1296afd2272b7d70b80ea093d193705
Alex Cipollone <alex.cipollone10@gmail.com>
2023-11-07 12:43:15 -0500

    tested reverse string queues

--- 07-stacks-queues-linked-lists/06-queue-implementation/queue-run.js
+++ 07-stacks-queues-linked-lists/06-queue-implementation/queue-run.js
@@ -1,29 +1,30 @@
-const Queue = require('./queue');
+const Queue = require("./queue");
 
 // Code that Brad inserted not in the video
 
-/*
 const queue = new Queue();
+// console.log(queue.isEmpty());
+// console.log(queue.isFull());
+// console.log(queue.enqueue(1));
+// console.log(queue.enqueue(2));
+// console.log(queue.enqueue(3));
+// console.log(queue.peek());
+// console.log(queue.dequeue());
+// console.log(queue.peek());
+// console.log(queue.getLength());
+
+queue.enqueue(1);
 console.log(queue.isEmpty());
-console.log(queue.isFull());
-console.log(queue.enqueue(1));
-console.log(queue.enqueue(2));
-console.log(queue.enqueue(3));
-console.log(queue.peek());
-console.log(queue.dequeue());
-console.log(queue.peek());
-console.log(queue.getLength());
-*/
 
-const queue = new Queue();
+// const queue = new Queue();
 
-queue.enqueue('a');
-queue.enqueue('b');
-queue.enqueue('c');
+// queue.enqueue('a');
+// queue.enqueue('b');
+// queue.enqueue('c');
 
-queue.dequeue();
-queue.dequeue();
+// queue.dequeue();
+// queue.dequeue();
 
-console.log(queue.peek());
+// console.log(queue.peek());
 
-console.log(queue)
+// console.log(queue)
