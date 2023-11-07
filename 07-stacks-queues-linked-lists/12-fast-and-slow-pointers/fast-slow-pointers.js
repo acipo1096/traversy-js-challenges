60b22242183d5c771dc529cb7628578f51836ff9
Alex Cipollone <alex.cipollone10@gmail.com>
2023-11-07 15:08:41 -0500

    completed pointers and linked lists

---
+++ 07-stacks-queues-linked-lists/12-fast-and-slow-pointers/fast-slow-pointers.js
@@ -0,0 +1,23 @@
+let a = 1;
+let b = a;
+a = 2;
+
+let c = { name: 'John' }
+let d = c;
+c.name = 'Brad';
+
+console.log(d.name);
+
+// Example
+function findMiddle(head) {
+    let slow = head;
+    let fast = head;
+
+    while (fast !== null && fast.next !== null) {
+        fast = fast.next.next;
+        slow = slow.next;
+
+    }
+
+    return slow;
+}
