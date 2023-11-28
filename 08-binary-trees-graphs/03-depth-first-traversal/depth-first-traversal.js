999101686c70364a31fa4ce0625961a9e3140fb1
Alex Cipollone <alex.cipollone10@gmail.com>
2023-11-28 13:31:50 -0500

    creating the tree

--- 08-binary-trees-graphs/03-depth-first-traversal/depth-first-traversal.js
+++ 08-binary-trees-graphs/03-depth-first-traversal/depth-first-traversal.js
@@ -1,6 +1,12 @@
-const Stack = require('./stack');
+const Stack = require("./stack");
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
 
 function depthFirstTraversal() {}
 
