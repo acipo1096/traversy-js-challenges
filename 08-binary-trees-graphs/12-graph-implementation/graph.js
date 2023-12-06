de2c3cababf4d9a76f992de6df796434fead89ee
Alex Cipollone <alex.cipollone10@gmail.com>
2023-12-06 12:55:16 -0500

    learn to spell "adjacency"

--- 08-binary-trees-graphs/12-graph-implementation/graph.js
+++ 08-binary-trees-graphs/12-graph-implementation/graph.js
@@ -1,39 +1,39 @@
 class Graph {
   constructor() {
-    this.adjacenyList = {};
+    this.adjacencyList = {};
   }
 
   addVertex(vertex) {
-    this.adjacenyList[vertex] = [];
+    this.adjacencyList[vertex] = [];
   }
 
   addEdge(vertex1, vertex2) {
-    this.adjacenyList[vertex1].push(vertex2);
-    this.adjacenyList[vertex2].push(vertex1);
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
   }
 
   removeEdge(vertex1, vertex2) {
-    this.adjacenyList[vertex1] = this.adjacenyList[vertex1].filter(
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
       (v) => v !== vertex2
     );
 
-    this.adjacenyList[vertex2] = this.adjacenyList[vertex2].filter(
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
       (v) => v !== vertex1
     );
   }
 
   removeVertex(vertex) {
-    while (this.adjacenyList[vertex].length) {
-      const adjacentVertex = this.adjacenyList[vertex].pop();
+    while (this.adjacencyList[vertex].length) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
       this.removeEdge(vertex, adjacentVertex);
     }
 
-    delete this.adjacenyList[vertex];
+    delete this.adjacencyList[vertex];
   }
 
   printAdjacencyList() {
-    for (const vertex in this.adjacenyList) {
-      console.log(`${vertex} -> ${this.adjacenyList[vertex].join(",")}`);
+    for (const vertex in this.adjacencyList) {
+      console.log(`${vertex} -> ${this.adjacencyList[vertex].join(", ")}`);
     }
   }
 }
