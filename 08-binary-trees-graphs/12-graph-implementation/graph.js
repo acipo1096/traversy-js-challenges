812487e7997d1810473189453691bbeb7b9605ed
Alex Cipollone <alex.cipollone10@gmail.com>
2023-12-05 21:25:40 -0500

    completed graph implementation

--- 08-binary-trees-graphs/12-graph-implementation/graph.js
+++ 08-binary-trees-graphs/12-graph-implementation/graph.js
@@ -1,4 +1,42 @@
-class Graph {}
+class Graph {
+  constructor() {
+    this.adjacenyList = {};
+  }
+
+  addVertex(vertex) {
+    this.adjacenyList[vertex] = [];
+  }
+
+  addEdge(vertex1, vertex2) {
+    this.adjacenyList[vertex1].push(vertex2);
+    this.adjacenyList[vertex2].push(vertex1);
+  }
+
+  removeEdge(vertex1, vertex2) {
+    this.adjacenyList[vertex1] = this.adjacenyList[vertex1].filter(
+      (v) => v !== vertex2
+    );
+
+    this.adjacenyList[vertex2] = this.adjacenyList[vertex2].filter(
+      (v) => v !== vertex1
+    );
+  }
+
+  removeVertex(vertex) {
+    while (this.adjacenyList[vertex].length) {
+      const adjacentVertex = this.adjacenyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+
+    delete this.adjacenyList[vertex];
+  }
+
+  printAdjacencyList() {
+    for (const vertex in this.adjacenyList) {
+      console.log(`${vertex} -> ${this.adjacenyList[vertex].join(",")}`);
+    }
+  }
+}
 
 // Example Adjacency List
 // {
