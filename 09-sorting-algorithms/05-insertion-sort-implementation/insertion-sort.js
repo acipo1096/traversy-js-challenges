8db66fb110279808c820b5b2f723f15d096dd531
Alex Cipollone <alex.cipollone10@gmail.com>
2024-01-05 14:10:16 -0500

    implemented insertion sort

--- 09-sorting-algorithms/05-insertion-sort-implementation/insertion-sort.js
+++ 09-sorting-algorithms/05-insertion-sort-implementation/insertion-sort.js
@@ -1,3 +1,18 @@
-function insertionSort() {}
+function insertionSort(arr) {
+    // The first element is always considered the sorted array
+    for (let i = 1; i < arr.length; i++) {
+        const currentElement = arr[i];
+        let j = i - 1;
+
+        while (j >= 0 && arr[j] > currentElement) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = currentElement;
+    }
+
+    return arr;
+}
 
 module.exports = insertionSort;
